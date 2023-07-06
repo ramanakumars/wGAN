26e0d53d19cd076dfdee53e6328f7227aad8b635
Ramanakumar Sankar <rsankar@umn.edu>
2023-07-06 16:34:20 -0500

    allowing for customizable output size

--- gan/model.py
+++ gan/model.py
@@ -3,17 +3,18 @@ from einops.layers.torch import Rearrange
 
 
 class UpConvLayer(nn.Sequential):
-    def __init__(self, in_channels, out_channels, activation, **kwargs):
+    def __init__(self, in_channels, out_channels, activation, norm=True, **kwargs):
         if activation == 'sigmoid':
             activation = nn.Sigmoid()
         elif activation == 'leakyrelu':
             activation = nn.LeakyReLU(0.2, True)
 
-        super().__init__(
-            nn.ConvTranspose2d(in_channels, out_channels, **kwargs),
-            nn.InstanceNorm2d(out_channels),
-            activation
-        )
+        layers = [nn.ConvTranspose2d(in_channels, out_channels, **kwargs)]
+        if norm:
+            layers += [nn.InstanceNorm2d(out_channels)]
+        layers += [activation]
+
+        super().__init__(*layers)
 
 
 class DownConvLayer(nn.Sequential):
@@ -32,21 +33,27 @@ class DownConvLayer(nn.Sequential):
 
 
 class Generator(nn.Sequential):
-    def __init__(self, n_z, n_layers=5, out_channels=3):
+    def __init__(self, n_z, norm=False, n_layers=5, out_channels=3, final_size=256):
         self.n_z = n_z
 
         layers = []
 
         prev_filt = 512
         for _ in range(n_layers):
-            layers.append(UpConvLayer(prev_filt, int(prev_filt / 2), activation='leakyrelu',
+            layers.append(UpConvLayer(prev_filt, int(prev_filt / 2), activation='leakyrelu', norm=norm,
                                       kernel_size=(6, 6), stride=(2, 2), padding=2))
             prev_filt = int(prev_filt / 2)
 
+        initial_size = final_size / 2 ** n_layers
+        if initial_size % 1 != 0:
+            raise ValueError(f"Cannot create a model to produce a {final_size} x {final_size} image with {n_layers} layers")
+
+        initial_size = int(initial_size)
+
         super().__init__(
-            nn.Linear(n_z, 8 * 8 * 512),
+            nn.Linear(n_z, initial_size * initial_size * 512),
             nn.LeakyReLU(0.2, True),
-            Rearrange('b (h w z) -> b z h w', h=8, w=8, z=512),
+            Rearrange('b (h w z) -> b z h w', h=initial_size, w=initial_size, z=512),
             *layers,
             nn.Conv2d(prev_filt, out_channels, (5, 5), stride=(1, 1), padding=2),
             nn.Sigmoid()
